3bd36ca3a95a718d7c593ca916cd390ee7f27ea8
Alexey <aleksey.panov@saritasa.com>
2022-07-04 16:50:56 +0700

    fixate changes

--- apps/react/src/App.tsx
+++ apps/react/src/App.tsx
@@ -1,18 +1,18 @@
 import { FC, Suspense } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { RootRouter } from './routes/RootRouter';
 import { store } from './store';
 
 export const App: FC = () => (
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <div>
         <Suspense fallback={<div>Brrr... here should be your loader component</div>}>
           <RootRouter />
         </Suspense>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
